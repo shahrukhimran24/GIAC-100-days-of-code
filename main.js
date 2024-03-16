ddfd5101282b209e02462a8348f0ef0b17200a29
shahrukhimran24 <160264746+shahrukhimran24@users.noreply.github.com>
2024-03-16 08:04:26 +0000

    Add files via upload

---
+++ main.js
@@ -0,0 +1,9 @@
+"use strict";
+// Q2:
+let personName = "shahrukh";
+console.log(`hello`, personName, "would you like to learn some type script today");
+// Q3:
+// let personName="shahrukh";
+console.log(personName.toLowerCase());
+console.log(personName.toUpperCase());
+console.log(personName.charAt(0).toUpperCase() + personName.slice(1).toLocaleLowerCase());
