dbad7fe0609e1d65a181ec3cff0f1268a68e4fba
shahrukhimran24 <160264746+shahrukhimran24@users.noreply.github.com>
2024-03-17 08:01:23 +0000

    Add files via upload

---
+++ day2/main.ts
@@ -0,0 +1,11 @@
+ // Q4
+console.log("Audrey Hypburn once said",'"word IMPOSSIBLE itself says I AM possible"');
+// Q5
+let famousperson="Auderey Hypburn";
+let message="once said,'word IPOSSIBLE itself says I AM possible'";
+console.log(famousperson,message);
+// Q6
+let whiteSpace="\n\t Shahrukh khan\t\n";
+console.log(whiteSpace);
+let withoutwhiteSpace=whiteSpace.trim();
+console.log(withoutwhiteSpace);
