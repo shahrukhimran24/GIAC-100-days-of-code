8418672cf25e61d2beb55dc9dd16c6da53bb1437
shahrukhimran24 <160264746+shahrukhimran24@users.noreply.github.com>
2024-03-18 07:50:21 +0000

    Add files via upload

---
+++ day4/main.js
@@ -0,0 +1,14 @@
+// Q10
+// let myName = "shahrukh";
+// console.log(`Hello ${myName},would you like to learn some TypeScipt today?`);
+// Q11
+// let friendNames = ["Haider","Shahmir","Ali"];
+// console.log(friendNames[0]);
+// console.log(friendNames[1]);
+// console.log(friendNames[2]);
+// Q12
+var friendName = ["Haider", "Shahmir", "Ali"];
+var message = "Would you like to learn some Typescript today";
+console.log("Hello", friendName[0], message);
+console.log("Hello", friendName[1], message);
+console.log("Hello", friendName[2], message);
---
+++ day4/main.ts
@@ -0,0 +1,14 @@
+// Q10
+let myName = "shahrukh";
+console.log(`Hello ${myName},would you like to learn some TypeScipt today?`);
+// Q11
+let friendNames = ["Haider","Shahmir","Ali"];
+console.log(friendNames[0]);
+console.log(friendNames[1]);
+console.log(friendNames[2]);
+// Q12
+let friendName =["Haider","Shahmir","Ali"]
+let message = "Would you like to learn some Typescript today"
+console.log("Hello",friendName[0],message)
+console.log("Hello",friendName[1],message)
+console.log("Hello",friendName[2],message)
